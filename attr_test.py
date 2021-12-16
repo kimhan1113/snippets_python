5e1cc268986d464718788e14bb842f00a37bce92
kimhan1113 <kimhan1113@naver.com>
2021-12-17 00:42:27 +0900

    attr isinstance commit

---
+++ attr_test.py
@@ -0,0 +1,35 @@
+
+
+class test:
+    a = 1
+    def hoxy(self):
+        num = 10
+        return num
+t = test()
+
+# t라는 객체에 있는 "hoxy" 라는 인스턴스를 쓰는법 오른쪽 괄호에는 피라미터값이 있으면 넣는다
+print(getattr(t, "hoxy")())
+
+# b라는 멥버가 있는지? true or false
+print(hasattr(t, 'b'))
+
+# t객체에서 a라는 변수를 찾아 5라는 값을 할당!!
+setattr(t, "a", 5)
+print(getattr(t, "a"))
+
+result1 = isinstance(100, int)
+# print(f"{result1}는 int 타입이 맞습니다.")
+
+
+class test1:
+    pass
+
+class test2(test1):
+    pass
+
+t2 = test2()
+
+result2 = isinstance(t2, test1)
+print(result2)
+
+
